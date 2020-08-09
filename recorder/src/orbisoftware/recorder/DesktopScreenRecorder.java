4784a14fb4fb2b281944bf1f381b763425c77d98
harlan3 <harlan3@hotmail.com>
2020-08-09 10:01:55 -0400

    Catpure unique for Windows Linux

--- recorder/src/orbisoftware/recorder/DesktopScreenRecorder.java
+++ recorder/src/orbisoftware/recorder/DesktopScreenRecorder.java
@@ -19,8 +19,11 @@ public class DesktopScreenRecorder extends ScreenRecorder {
 
 	public static boolean useWhiteCursor;
 	private Image mouseCursor;
+
+	private String myOS = System.getProperty("os.name").toLowerCase();
 	
-	public DesktopScreenRecorder(Display display, Rectangle rect, OutputStream oStream, ScreenRecorderListener listener) {
+	public DesktopScreenRecorder(Display display, Rectangle rect, OutputStream oStream,
+			ScreenRecorderListener listener) {
 		super(rect, oStream, listener);
 
 		try {
@@ -34,27 +37,36 @@ public class DesktopScreenRecorder extends ScreenRecorder {
 			File file = new File("mouse_cursors" + File.separator + mouseCursorFile);
 			ImageData[] cursorImageData = new ImageLoader().load(file.getAbsolutePath());
 			mouseCursor = new Image(display, cursorImageData[0]);
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+	public boolean isWindows() {
+
+		return (myOS.indexOf("win") >= 0);
+	}
+	
 	public Image captureScreen(Display display, Rectangle recordArea) {
 
 		Point mousePosition = MouseInfo.getPointerInfo().getLocation();
 		mousePosition.x = mousePosition.x - recordArea.x;
 		mousePosition.y = mousePosition.y - recordArea.y;
-
-		PaletteData palette = new PaletteData(0xFF0000,0xFF00, 0xFF);
-                ImageData imageData = new ImageData(recordArea.width, recordArea.height, 24, palette);
-                
-		final Image swtImage = new Image(display, imageData);
+		Image swtImage;
+		
+		if (isWindows()) {
+			PaletteData palette = new PaletteData(0xFF0000, 0xFF00, 0xFF);
+			ImageData imageData = new ImageData(recordArea.width, recordArea.height, 24, palette);
+			swtImage = new Image(display, imageData);
+		}
+		else
+			swtImage = new Image(display, recordArea);
 		
 		final GC gc = new GC(display);
 		gc.copyArea(swtImage, recordArea.x, recordArea.y);
 		gc.dispose();
-		  
+
 		final GC gc2 = new GC(swtImage);
 		gc2.drawImage(mouseCursor, mousePosition.x - 8, mousePosition.y - 5);
 		gc2.dispose();
